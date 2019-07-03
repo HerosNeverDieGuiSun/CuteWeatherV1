4f9ac4f035469786b0565170d199bbe15a6dac1b
RoyChengC <919275390@qq.com>
2019-07-03 17:20:37 +0800

    导航栏

--- app/src/main/java/com/example/cuteweatherv1/ui/FragmentAir.kt
+++ app/src/main/java/com/example/cuteweatherv1/ui/FragmentAir.kt
@@ -2,12 +2,14 @@ package com.example.cuteweatherv1.ui
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.example.cuteweatherv1.R
+import kotlinx.android.synthetic.main.fragment_air.*
 
 class FragmentAir : Fragment() {
 
@@ -21,6 +23,7 @@ class FragmentAir : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         return inflater.inflate(R.layout.fragment_air, container, false)
     }
 
@@ -28,6 +31,13 @@ class FragmentAir : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(FragmentAirViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        cv.setOnClickListener {
+            Log.e("my", "-----------------")
+        }
     }
 
 }
--- app/src/main/java/com/example/cuteweatherv1/ui/MainActivity.kt
+++ app/src/main/java/com/example/cuteweatherv1/ui/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.example.cuteweatherv1.ui
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.cuteweatherv1.R
+import com.readystatesoftware.systembartint.SystemBarTintManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //applyKitKatTranslucency()
     }
+
+    /*private fun applyKitKatTranslucency() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            setTranslucentStatus(true)
+            val mTintManager = SystemBarTintManager(this)
+            mTintManager.isStatusBarTintEnabled = true
+            //mTintManager.setStatusBarTintResource(R.color)
+        }
+    }
+
+    private fun setTranslucentStatus(state : Boolean) {
+        val layoutParams = window.attributes
+
+    }*/
+
 }

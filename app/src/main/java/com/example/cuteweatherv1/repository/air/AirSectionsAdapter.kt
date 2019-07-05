b21a54cca246cabb94114c4b0446e402bbee69e7
zzd <949759694@qq.com>
2019-07-05 10:14:12 +0800

    完善空气质量界面

--- app/src/main/java/com/example/cuteweatherv1/repository/air/AirSectionsAdapter.kt
+++ app/src/main/java/com/example/cuteweatherv1/repository/air/AirSectionsAdapter.kt
@@ -8,11 +8,15 @@ import com.example.cuteweatherv1.R
 import com.example.cuteweatherv1.ui.air.FragmentAirInfo
 
 private val TAB_TITLES = arrayOf(
+    R.string.tab_text_7,
     R.string.tab_text_1,
     R.string.tab_text_2,
     R.string.tab_text_3,
     R.string.tab_text_4,
-    R.string.tab_text_5
+    R.string.tab_text_5,
+    R.string.tab_text_6,
+    R.string.today,
+    R.string.tomorrow
 )
 class AirSectionsAdapter (private val context: Context, fm: FragmentManager) : FragmentPagerAdapter(fm){
 
@@ -23,7 +27,17 @@ class AirSectionsAdapter (private val context: Context, fm: FragmentManager) : F
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
-        return context.resources.getString(TAB_TITLES[position])
+        val c = ChangeToWeek()
+        val week = c.toWeek()
+        if (position == 0) {
+            return context.resources.getString(TAB_TITLES[7])
+        } else if (position ==1) {
+            return context.resources.getString(TAB_TITLES[8])
+        } else if (position + week <= 7) {
+            return context.resources.getString(TAB_TITLES[position+week-1])
+        } else{
+            return context.resources.getString(TAB_TITLES[position+week-8])
+        }
     }
 
     override fun getCount(): Int {
